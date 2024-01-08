b54d0a7244e947c2ab7551355ae25e8a6c09f5a6
Manilko <56886797+Manilko@users.noreply.github.com>
2024-01-09 01:11:06 +0200

    plist

--- TokaWorld-18/Models/ItemModel.swift
+++ TokaWorld-18/Models/ItemModel.swift
@@ -29,25 +29,25 @@ extension ItemModel{
         self.isPopular = false
     }
 }
-
-struct MocDate{
-    
-    let collection: [ItemModel]
-    
-    
-}
-
-extension MocDate{
-    
-    init(){
-        let item = ItemModel(title: "Set Name", icon: "mocImage", discription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id adipiscing elit.")
-        self.collection = [item, item, item, item, item, item, item]
-    }
-    
-    init(complex: Bool = true){
-        let itemFavorite = ItemModel(title: "Set Name", icon: "mocImage", discription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id adipiscing elit.", isFavorite: true, isNew: false, isPopular: false)
-        let itemNew = ItemModel(title: "Set Name", icon: "mocImage", discription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id adipiscing elit.", isFavorite: false, isNew: true, isPopular: false)
-        let itemPopular = ItemModel(title: "Set Name", icon: "mocImage", discription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id adipiscing elit.", isFavorite: false, isNew: false, isPopular: true)
-        self.collection = [itemFavorite, itemFavorite, itemFavorite, itemFavorite, itemFavorite, itemFavorite, itemFavorite,itemNew,itemNew,itemNew,itemNew,itemPopular,itemPopular,itemPopular,itemPopular]
-    }
-}
+//
+//struct MocDate{
+//    
+//    let collection: [ItemModel]
+//    
+//    
+//}
+//
+//extension MocDate{
+//    
+//    init(){
+//        let item = ItemModel(title: "Set Name", icon: "mocImage", discription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id adipiscing elit.")
+//        self.collection = [item, item, item, item, item, item, item]
+//    }
+//    
+//    init(complex: Bool = true){
+//        let itemFavorite = ItemModel(title: "Set Name", icon: "mocImage", discription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id adipiscing elit.", isFavorite: true, isNew: false, isPopular: false)
+//        let itemNew = ItemModel(title: "Set Name", icon: "mocImage", discription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id adipiscing elit.", isFavorite: false, isNew: true, isPopular: false)
+//        let itemPopular = ItemModel(title: "Set Name", icon: "mocImage", discription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id adipiscing elit.", isFavorite: false, isNew: false, isPopular: true)
+//        self.collection = [itemFavorite, itemFavorite, itemFavorite, itemFavorite, itemFavorite, itemFavorite, itemFavorite,itemNew,itemNew,itemNew,itemNew,itemPopular,itemPopular,itemPopular,itemPopular]
+//    }
+//}
